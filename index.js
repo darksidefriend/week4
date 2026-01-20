0da0d496ea622d3ae5e1581d70f894e3b9866bad
Daniil Savelev <dansavels@yandex.ru>
2026-01-20 16:35:07 +0300

    week 8 try

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-// index.js
-import express from "express";
-import { createReadStream } from "fs";
-import crypto from "crypto";
-import http from "http";
-import bodyParser from "body-parser";
+const express = require('express');
+const axios = require('axios');
+const pug = require('pug');
 
-import appSrc from "./app.js";
+const https = require('https');
+const fs = require('fs');
 
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
+// TODO: Добавьте ваш логин
+const login = '23886bd5-1b0d-4860-8ed8-d9106051b1a1';
 
-app.listen(3000);
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+app.get('/login/', (_, res) => {
+  res.send(login);
+});
+
+app.get('/wordpress/wp-json/wp/v2/posts/1', (_, res) => {
+  res.json({
+    id: 1,
+    slug: login,
+    title: {
+      rendered: login
+    },
+    content: {
+      rendered: "",
+      protected: false
+    }
+  });
+});
+
+app.use(express.json());
+
+app.post('/render/', async (req, res) => {
+  const { random2, random3 } = req.body;
+  const { addr } = req.query;
+
+  const templateResponse = await axios.get(addr);
+  const pugTemplate = templateResponse.data;
+
+  const compiled = pug.compile(pugTemplate);
+  const html = compiled({ random2, random3 });
+
+  res.set('Content-Type', 'text/html');
+  res.send(html);
+});
+
+const PORT = 443;
+
+// TODO: Добавьте пути к вашим сертификатам
+const options = {
+  key: fs.readFileSync('/your-key-path/privkey.pem'),
+  cert: fs.readFileSync('/your-cert-path/fullchain.pem')
+};
+
+const server = https.createServer(options, app);
+
+server.listen(PORT);
