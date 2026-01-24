220fb3a5805723f624cbf32f5f0aebfb51fe3408
darksidefriend <dansavels@yandex.ru>
2026-01-24 13:45:18 +0300

    cors fix

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const app = express();
 
 /* CORS */
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    '*'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,OPTIONS'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
   next();
 });
 
