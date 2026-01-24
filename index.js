2b1b81e6a34279aec6dfedf8c0974f4a042c9c4c
darksidefriend <dansavels@yandex.ru>
2026-01-24 13:33:53 +0300

    week 7 try

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { createReadStream } from "fs";
-import crypto from "crypto";
-import http from "http";
+const express = require('express');
+const { MongoClient } = require('mongodb');
 
-import appSrc from "./app.js";
+const app = express();
 
-const app = appSrc(
-  express,
-  bodyParser,
-  createReadStream,
-  crypto,
-  http
-);
+/* CORS */
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
 
-app.listen(process.env.PORT || 3000);
+/* body parser */
+app.use(express.urlencoded({ extended: true }));
+
+/* /login */
+app.get('/login/', (_, res) => {
+  res.send('23886bd5-1b0d-4860-8ed8-d9106051b1a1'); // ← сюда свой логин
+});
+
+/* /insert */
+app.post('/insert/', async (req, res) => {
+  let client;
+
+  try {
+    const { login, password, URL } = req.body;
+
+    client = new MongoClient(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    await client.connect();
+
+    const dbName = URL.split('/').pop().split('?')[0];
+    const db = client.db(dbName);
+    const users = db.collection('users');
+
+    await users.insertOne({
+      login: String(login),
+      password: String(password)
+    });
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  } finally {
+    if (client) await client.close();
+  }
+});
+
+/* Render PORT */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
